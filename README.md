ddb907b8c7436ed6283844f434960ac61cb7f59c
Andre Barros <andre.barros@indiecampers.com>
2025-05-09 03:32:06 -0300

    add test to create refueling for user without balance

--- README.md
+++ README.md
@@ -6,19 +6,10 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
+ ruby 3.4.3 (2025-04-14 revision d0b7e5b6a0) +PRISM [x86_64-linux]
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
+* Database creation and seed
+''' bundle exec rake db:create db:migrate db:seed
 
 * How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+''' bundle exec rspec -fd ./spec
--- spec/services/refuelings_services/create_spec.rb
+++ spec/services/refuelings_services/create_spec.rb
@@ -13,20 +13,38 @@ describe RefuelingsServices::Create do
   describe "#perform" do
     subject(:service_result) { described_class.new(params).perform }
 
-    context "create refuelings and deduce user balance" do
+    context "user with balance" do
       it "create refuelings" do
         refueling = service_result
 
         expect(Refueling.where(user: user, gasstation: gasstation).last).to eq refueling
       end
 
-      it "deduce user balance " do
+      it "deduce user balance" do
         user_balance_before = user.balance
         refueling = service_result
         user.reload
 
         expect(user.balance).to be < user_balance_before
         
+      end
+    end
+
+    context "user without balance" do
+      before do
+        user.update(balance: 0)
+      end
+
+      it "does not create refuelings" do
+        service_result
+
+        expect(Refueling.where(user: user, gasstation: gasstation).count).to eq 0
+      end
+
+      it "returns error message" do
+        service_result
+
+        expect(service_result.errors[:base]).to include("User balance is not enough")
       end
     end
   end
